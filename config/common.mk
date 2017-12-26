nitrogen-project/android_vendor_nitrogen

Add in dialer_experience.xml to fix google phone bitching at us

products/common.mk
@@ -42,6 +42,10 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES := \
 PRODUCT_PACKAGE_OVERLAYS += \
     vendor/nitrogen/overlay/common
 
+# Fix Dialer
+PRODUCT_COPY_FILES +=  \
+    vendor/nitrogen/prebuilt/common/etc/sysconfig/dialer_experience.xml:system/etc/sysconfig/dialer_experience.xml
+
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml

Ninja-OS/vendor_ninja

Change-Id: I99adfb8d70b971fdd47afc76e70303e4edf982cc

config/common.mk
@@ -132,6 +132,10 @@ PRODUCT_COPY_FILES +=  \
 PRODUCT_COPY_FILES += \
     vendor/ninja/prebuilt/common/lib/content-types.properties:system/lib/content-types.properties
 
+# Fix Dialer
+PRODUCT_COPY_FILES +=  \
+    vendor/aosp/prebuilt/common/etc/sysconfig/dialer_experience.xml:system/etc/sysconfig/dialer_experience.xml
+
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml