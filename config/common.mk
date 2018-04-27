nychitman1/android_vendor_du

Include common overlays
Change-Id: I3b7682ab9fcdab1018bf2be1c48b06e5e21c5391

config/common.mk
@@ -52,3 +52,6 @@ include vendor/du/config/branding.mk
 
 # Google sounds
 include vendor/du/google/GoogleAudio.mk
+
+# Overlays
+PRODUCT_PACKAGE_OVERLAYS += vendor/du/overlay/common

Ninja-OS/vendor_ninja

config/common.mk
@@ -48,3 +48,6 @@ include vendor/ninja/config/packages.mk
 
 # NinjaVersion
 include vendor/ninja/config/ninja.mk
+
+# Overlays
+PRODUCT_PACKAGE_OVERLAYS += vendor/ninja/overlay/common