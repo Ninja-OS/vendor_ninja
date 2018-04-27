AospExtended/platform_vendor_aosp

Import charging sounds
Extracted from this commit
https://android-review.googlesource.com/#/c/platform/frameworks/base/+/243420/

Change-Id: Idba17bb1b6d2f6899bcd98d2fe7336d31835cb42

config/common.mk
@@ -130,6 +130,12 @@ PRODUCT_PACKAGES += \
     libffmpeg_omx \
     media_codecs_ffmpeg.xml
 
+# Charging sounds
+PRODUCT_COPY_FILES += \
+    vendor/aosp/google/effects/BatteryPlugged.ogg:system/media/audio/ui/BatteryPlugged.ogg \
+    vendor/aosp/google/effects/BatteryPlugged_48k.ogg:system/media/audio/ui/BatteryPlugged_48k.ogg
+
+
 # Needed by some RILs and for some gApps packages
 PRODUCT_PACKAGES += \
     librsjni \

Ninja-OS/vendor_ninja

config/common.mk
@@ -48,6 +48,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/ninja/prebuilt/common/etc/mkshrc:system/etc/mkshrc
 
+# Charging sounds
+PRODUCT_COPY_FILES += \
+    vendor/ninja/google/effects/BatteryPlugged.ogg:system/media/audio/ui/BatteryPlugged.ogg \
+    vendor/ninja/google/effects/BatteryPlugged_48k.ogg:system/media/audio/ui/BatteryPlugged_48k.ogg
+
 # Packages
 include vendor/ninja/config/packages.mk
 